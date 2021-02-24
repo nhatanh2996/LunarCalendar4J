1b5ef2b169276b2e39918bd292a645318e7efe7c
nhatanhfaber <nguyenducnhatanh@fabercompany.co.jp>
2021-02-24 22:38:28 +0700

    new year

--- src/main/java/dto/DiaChi.java
+++ src/main/java/dto/DiaChi.java
@@ -1,27 +1,40 @@
 package dto;
 
+import java.util.Arrays;
+
 public enum DiaChi {
-    TI("Tý"),
-    SUU("Sửu"),
-    DAN("Dần"),
-    MAO("Mão"),
-    THIN("Thìn"),
-    TY("Tỵ"),
-    NGO("Ngọ"),
-    MUI("Mùi"),
-    THAN("Thân"),
-    DAU("Dậu"),
-    TUAT("Tuất"),
-    HOI("Hợi"),
+    TI(1, "Tý"),
+    SUU(2, "Sửu"),
+    DAN(3, "Dần"),
+    MAO(4, "Mão"),
+    THIN(5, "Thìn"),
+    TY(6, "Tỵ"),
+    NGO(7, "Ngọ"),
+    MUI(8, "Mùi"),
+    THAN(9, "Thân"),
+    DAU(10, "Dậu"),
+    TUAT(11, "Tuất"),
+    HOI(12, "Hợi"),
     ;
 
-    DiaChi(String name) {
+    DiaChi(int code, String name) {
         this.name = name;
+        this.code = code;
     }
 
     private final String name;
+    private final int code;
 
     public String getName() {
         return name;
     }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static DiaChi fromCode(int code) {
+        return Arrays.stream(values()).filter(diaChi -> diaChi.getCode() == code).findFirst().orElse(null);
+    }
+
 }
